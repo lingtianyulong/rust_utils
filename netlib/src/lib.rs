7f0f935338cc8cb6a142836369808a2f95254189
lingtianyulong <lingtianyulong@126.com>
2025-07-19 16:41:55 +0800

    1， 封装 netlib 模块，在 Qt 中进行 http post 请求, 注册用户信息;

--- netlib/src/lib.rs
+++ netlib/src/lib.rs
@@ -1,11 +1,15 @@
-use std::ffi::{ CStr, CString};
-use libc::{c_char, c_int};
+use std::error::Error;
+use std::ffi::{CStr, CString};
+use std::time::Duration;
+use libc::{c_char};
 use reqwest::blocking::Client;
-use reqwest::header::CONTENT_TYPE;
+use reqwest::header::{HeaderValue, CONTENT_TYPE, USER_AGENT};
 
 // 发送 POST 请求, 参数为 url 和 json 字符串
 #[unsafe(no_mangle)]
 pub extern "C" fn http_post(url: *const c_char, body:* const c_char) -> *mut c_char {
+    println!("In http post");
+
     if url.is_null() {
         return std::ptr::null_mut();
     }
@@ -24,22 +28,47 @@ pub extern "C" fn http_post(url: *const c_char, body:* const c_char) -> *mut c_c
         Err(_) => return std::ptr::null_mut(),
     };
 
-    let client = Client::new();
-    let response = match client
-        .post(url_str)
-        .header(CONTENT_TYPE, "application/json")
-        .body(json_body.to_string())
-        .send()
-    {
-        Ok(response) => match response.text() {
-            Ok(body) => body,
-            Err(_) => return std::ptr::null_mut(),
-        } ,
-        Err(_) => return std::ptr::null_mut(),
-    };
+    println!("url_str: {}", url_str);
+    println!("json_body: {}", json_body);
 
-    let c_str = CString::new(response.as_str()).unwrap();
-    c_str.into_raw()
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    headers.insert(USER_AGENT, HeaderValue::from_static("curl/7.88.0"));
+
+    let client = Client::builder()
+        .no_proxy()
+        .default_headers(headers)
+        .timeout(Duration::from_secs(20))
+        .build().unwrap();
+
+    let res = client
+        .post(url_str)
+        .header("Content-Type", "application/json")
+        .body(json_body.to_string())
+        .send();
+
+    match res {
+        Ok(resp) => {
+            let text = resp.text();
+            match text {
+                Ok(body) => CString::new(body).unwrap().into_raw(),
+                Err(e) => {
+                    eprintln!("Failed to read body: {}", e);
+                    std::ptr::null_mut()
+                }
+            }
+        }
+        Err(e) => {
+            // 打印详细错误链
+            eprintln!("POST failed: {}", e);
+            let mut source = e.source();
+            while let Some(inner) = source {
+                eprintln!("Caused by: {}", inner);
+                source = inner.source();
+            }
+            std::ptr::null_mut()
+        }
+    }
 }
 
 // 释放返回的字符串
